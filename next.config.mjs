24195c30efdf9a11c59dcc9f02d868b4a3853363
brown.dev <brown.dev@brown.com.kh>
2026-09-11 10:38:12 +0700

    Configure GitHub Pages deployment and static search

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,11 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/e-learning-implementation-playbook',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default withMDX(config);
